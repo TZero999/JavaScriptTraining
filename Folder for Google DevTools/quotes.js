b4681398fafeb99d287ee9bdabb62092cf088c7e
TZero <jaroslawandolegskoro@gmail.com>
2022-08-24 16:06:49 +0300

    git remote add origin https://github.com/TZero999/JavaScriptTraining.git

---
+++ Folder for Google DevTools/quotes.js
@@ -0,0 +1,9 @@
+let string1 ='Кавычки одинарные';
+let string2 ="Кавычки двойные";
+let string3 =`Обратные кавычки позволяют встраивать переменные. ${example}`;
+let example ='Например, вот так.'
+
+alert (string1);
+alert (string2);
+alert (string3);
+alert ("А вот что было в переменной example:", $example);
